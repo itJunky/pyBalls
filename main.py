c0f426af857edada2ea95923308b01317a9cf67e
itjunky <itjunky@github.com>
2022-02-06 19:41:26 +0300

    Fix right and bottom wall reduce

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
 import random
 import time
+import sys
+import pygame
 
-import pygame, sys
-
+balls_count = 10
+ball_size = 15
 pygame.init()
+balls = []
 black = 0, 0, 0
-size = width, height = 1000, 800
+size = scr_width, scr_height = 1400, 800
 screen = pygame.display.set_mode(size)
 
 
 class Ball:
-    def __init__(self, first_x, first_y, speed=5):
+    def __init__(self, first_x, first_y, speed=5, bsz=25):
         ball_img = pygame.image.load("intro_ball.gif").convert_alpha()
-        bsz = 25
         bl_size = (bsz, bsz)
         self.ball_img = pygame.transform.scale(ball_img, bl_size)
         self.speed = [speed, speed]
         if first_x or first_y:
-            self.rect = ball_img.get_rect(topleft=(first_x, first_y))
+            self.rect = ball_img.get_rect(topleft=(first_x, first_y), size=bl_size)
         else:
             self.rect = ball_img.get_rect(size=bl_size)
 
     def show(self):
         self.rect = self.rect.move(self.speed)
-        self.change_direction() # if needed
+        self.change_direction()  # if needed
         screen.blit(self.ball_img, self.rect)
 
-
     def change_direction(self):
-        if self.rect.left < 0 or self.rect.right > width:
+        if self.rect.left < 0 or self.rect.right > scr_width:
             self.speed[0] = -self.speed[0]
 
-        if self.rect.top < 0 or self.rect.bottom > height:
+        if self.rect.top < 0 or self.rect.bottom > scr_height:
             self.speed[1] = -self.speed[1]
 
 
@@ -40,9 +41,13 @@ if __name__ == '__main__':
     name = 'Baller'
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-    balls = []
-    for i in range(30):
-        balls.append(Ball(random.randint(1, width-100), random.randint(1, height-100), random.randint(1, 9)))
+    for i in range(balls_count):
+        balls.append(Ball(
+            random.randint(1, scr_width-ball_size),
+            random.randint(1, scr_height-ball_size),
+            random.randint(1, 9),
+            bsz=ball_size)
+        )
 
     while 1:
         for event in pygame.event.get():
@@ -54,5 +59,3 @@ if __name__ == '__main__':
         pygame.display.flip()
         screen.fill(black)
         time.sleep(20/1000)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
