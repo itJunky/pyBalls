45afc58c8ea2292710c92f9e24c9aa586724fb06
itjunky <itjunky@github.com>
2022-02-06 17:05:00 +0300

    Ball as object and can drop many randomized balls

--- main.py
+++ main.py
@@ -1,54 +1,56 @@
+import random
 import time
 
 import pygame, sys
 
 pygame.init()
-size = width, height = 800, 600
-speed = [2, 2]
-speed2 = [3, 3]
-speed3 = [4, 4]
+size = width, height = 1000, 800
 black = 0, 0, 0
 screen = pygame.display.set_mode(size)
-ball_img = pygame.image.load("intro_ball.gif").convert_alpha()
-bl_size = (15, 15)
-ball_img = pygame.transform.scale(ball_img, bl_size)
-ball = ball_img.get_rect(size=bl_size)
-
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def show(ballrect, speed):
-    # Use a breakpoint in the code line below to debug your script.
+class Ball:
+    def __init__(self, first_x, first_y, speed=5):
+        ball_img = pygame.image.load("intro_ball.gif").convert_alpha()
+        bsz = 25
+        bl_size = (bsz, bsz)
+        self.ball_img = pygame.transform.scale(ball_img, bl_size)
+        self.speed = [speed, speed]
+        if first_x or first_y:
+            self.rect = ball_img.get_rect(topleft=(first_x, first_y))
+        else:
+            self.rect = ball_img.get_rect(size=bl_size)
 
-    ballrect = ballrect.move(speed)
+    def show(self):
+        self.rect = self.rect.move(self.speed)
+        self.change_direction() # if needed
+        screen.blit(self.ball_img, self.rect)
+        pygame.display.flip()
 
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
+    def change_direction(self):
+        if self.rect.left < 0 or self.rect.right > width:
+            self.speed[0] = -self.speed[0]
 
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
-    screen.blit(ball_img, ballrect)
-    pygame.display.flip()
-
-    return ballrect, speed
+        if self.rect.top < 0 or self.rect.bottom > height:
+            self.speed[1] = -self.speed[1]
 
 
-# Press the green button in the gutter to run the script.
+# Start main circle of program
 if __name__ == '__main__':
     name = 'Baller'
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-    ball = ball_img.get_rect(topleft=(100, 350), size=bl_size)
-    ball2 = ball_img.get_rect(topleft=(200, 300), size=bl_size)
-    ball3 = ball_img.get_rect(topleft=(300, 300), size=bl_size)
+    balls = []
+    for i in range(30):
+        balls.append(Ball(random.randint(1, width-100), random.randint(1, height-100), random.randint(1, 9)))
+
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
 
-        ball, speed = show(ball, speed)
-        ball2, speed2 = show(ball2, speed2)
-        ball3, speed3 = show(ball3, speed3)
+        for ball in balls:
+            ball.show()
 
         screen.fill(black)
         time.sleep(20/1000)
